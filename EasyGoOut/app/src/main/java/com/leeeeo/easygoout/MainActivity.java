e5b3e391e1d87399113b5efbe0bf47370be8b07f
Leo Liu <1010820319@qq.com>
2017-07-22 11:33:57 +0800

    add main page layout

--- EasyGoOut/app/src/main/java/com/leeeeo/easygoout/MainActivity.java
+++ EasyGoOut/app/src/main/java/com/leeeeo/easygoout/MainActivity.java
@@ -2,13 +2,20 @@ package com.leeeeo.easygoout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.user_center_page);
+
+
+//        //自定义标题
+//        setContentView(R.layout.user_center_page);
+////设置标题为某个layout
+//        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
     }
 
 }
