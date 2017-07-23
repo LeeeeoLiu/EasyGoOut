27421218a7f0b0cb8af689e3c465286433aa9daf
Leo Liu <1010820319@qq.com>
2017-07-23 10:04:59 +0800

    add texi_spot_query and suggest result page

--- EasyGoOut/app/src/main/java/com/leeeeo/easygoout/MainActivity.java
+++ EasyGoOut/app/src/main/java/com/leeeeo/easygoout/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.text_spot_query);
 
 
 //        //自定义标题
